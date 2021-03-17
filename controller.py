36e1bd2f3b5b6f2ce57a7e72568d3740c4c7e54a
thelordahmed <ahmed.lord100@gmail.com>
2021-03-17 23:12:14 +0200

    1.0.1 stable

--- controller.py
+++ controller.py
@@ -9,6 +9,6 @@ else:
 browser_folder = os.path.join(data_folder, "browserData")
 fb_processed_json = os.path.join(data_folder, "fb_processed_pages.json")
 
-version = "1.0.0"
+version = "1.0.1"
 copyright_text = "AhmeDSaeeD | (lordahmed on Fiverr)"
 copyright_url = "https://fiverr.com/lordahmed"
